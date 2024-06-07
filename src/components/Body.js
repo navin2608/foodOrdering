92c6c1da0ad3455458b236253d61f3481b4abfee
navin.kumar <navin.kumar@i-exceed.com>
2024-06-07 19:27:36 +0530

    use effect added for dynamically fetch the data from api

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,21 +3,26 @@ import resObj from "../../utils/mockData";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { async } from "regenerator-runtime";
+import Shimmer from "./Shimmer";
 const Body=()=>{
-    const [listOfRestaurants,setListOfRestaurant]=useState(resObj);
+    const [listOfRestaurants,setListOfRestaurant]=useState([]);
     useEffect(()=>{
        fetchData();
     },[])
     const fetchData=async()=>{
-        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.076008&lng=72.8776707&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json=await data.json();
         console.log(json);
+        setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+       
     }
-    return (
+    //conditional Rendering
+    return listOfRestaurants.length===0?<Shimmer/>:
+   (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
-                    const filteredList=listOfRestaurants.filter(data=>data.info.avgRating>4.3)
+                    const filteredList=listOfRestaurants.filter(data=>data.info.avgRating>4.4)
                     console.log(listOfRestaurants)
                     // console.log("setListOfRestaurant-->",setListOfRestaurant)
                     setListOfRestaurant(filteredList);
